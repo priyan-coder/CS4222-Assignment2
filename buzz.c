239dca506829fd18964a5a677ff94d842fecbcb3
Priyan Rajamohan <rajamohanpriyan@gmail.com>
2022-02-09 21:59:04 +0800

    this seems to work

--- buzz.c
+++ buzz.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include "sys/etimer.h"
 #include "buzzer.h"
+#include "stdlib.h"
 
 /* Thresholds for picking up*/
 int ACC_X = 20;  // >
@@ -21,7 +22,7 @@ PROCESS(process_light, "light");      // declare r-timer for light sensor
 AUTOSTART_PROCESSES(&process_rtimer); // start r-timer-IMU on system boot
 
 /* IMU */
-static int counter_rtimer;                                 // counter var for r-timer - IMU
+// static int counter_rtimer;                                 // counter var for r-timer - IMU
 static struct rtimer timer_rtimer;                         // create a task
 static rtimer_clock_t timeout_rtimer = RTIMER_SECOND / 20; // 20 Hz
 static bool moved = false;                                 // movement flag
@@ -31,18 +32,20 @@ static int moveCount = 0;                                  // counter for detect
 int buzzerFrequency = 3951; // hgh notes on a piano
 
 /* Light Sensor */
-static int counter_light;                                // counter var for light sensor
+// static int counter_light;                                // counter var for light sensor
 static struct rtimer light_timer;                        // create a task
 static rtimer_clock_t timeout_light = RTIMER_SECOND / 4; // 4Hz
-static int prevValue;                                    // var to hold prev Lux value
+static int prevValue = 0;                                // var to hold prev Lux value
 static bool lightChange = false;                         // flag var to indicate significant light change
 static int count_exec = 0;                               // to account for prevValue not derived on first time of execution
+static int value = 0;
+static int diff = 0;
 
 static void init_mpu_reading(void);
 static void get_mpu_reading(int arr[]);
 
 static void init_opt_reading(void);
-static int get_light_reading(void);
+static void get_light_reading();
 
 /*
 static void
@@ -53,7 +56,6 @@ print_mpu_reading(int reading)
         printf("-");
         reading = -reading;
     }
-
     printf("%d.%02d", reading / 100, reading % 100);
 }
 */
@@ -62,31 +64,23 @@ print_mpu_reading(int reading)
 void do_rtimer_timeout(struct rtimer *timer, void *ptr)
 {
 
-    /* rtimer period 50ms = 20Hz*/
-    int sensor[6];
-    // gx, gy, gz, ax, ay, az
-    // read the IMU sensor every 0.05s i.e. 20Hz, 20 Samples per second
-    rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0, do_rtimer_timeout, NULL);
-
-    // int s, ms1, ms2, ms3;
-    // s = clock_time() / CLOCK_SECOND;
-    // ms1 = (clock_time() % CLOCK_SECOND) * 10 / CLOCK_SECOND;
-    // ms2 = ((clock_time() % CLOCK_SECOND) * 100 / CLOCK_SECOND) % 10;
-    // ms3 = ((clock_time() % CLOCK_SECOND) * 1000 / CLOCK_SECOND) % 10;
-
-    counter_rtimer++;
-    // printf(": %d (cnt) %d (ticks) %d.%d%d%d (sec) \n", counter_rtimer, clock_time(), s, ms1, ms2, ms3);
-    printf("Getting IMU readings\n");
-    get_mpu_reading(sensor);
-    if (sensor[0] < GYRO_X || sensor[1] > GYRO_Y || sensor[2] > GYRO_Z || sensor[3] > ACC_X || sensor[4] < ACC_Y || sensor[5] > ACC_Z)
+    if (!moved)
     {
-        moveCount += 1;
-        if (moveCount > 10)
+        /* rtimer period 50ms = 20Hz*/
+        int sensor[6];
+        // gx, gy, gz, ax, ay, az
+        // read the IMU sensor every 0.05s i.e. 20Hz, 20 Samples per second
+        rtimer_set(&timer_rtimer, RTIMER_NOW() + timeout_rtimer, 0, do_rtimer_timeout, NULL);
+        get_mpu_reading(sensor);
+        if (sensor[0] < GYRO_X || sensor[1] > GYRO_Y || sensor[2] > GYRO_Z || sensor[3] > ACC_X || sensor[4] < ACC_Y || sensor[5] > ACC_Z)
         {
-            moved = true;
-            moveCount = 0;
-            printf("Detected Motion. Stopped IMU\n");
-            return;
+            moveCount += 1;
+            if (moveCount > 10)
+            {
+                moved = true;
+                moveCount = 0;
+                printf("Detected Motion. Stopped IMU\n");
+            }
         }
     }
 }
@@ -94,33 +88,12 @@ void do_rtimer_timeout(struct rtimer *timer, void *ptr)
 /* Light callback */
 void do_light(struct rtimer *timer, void *ptr)
 {
-    /* Re-arm rtimer. Starting up the sensor takes around 125ms */
-
-    int v, diff;
-
-    rtimer_set(&light_timer, RTIMER_NOW() + timeout_light, 0, do_light, NULL);
-
-    // int s, ms1, ms2, ms3;
-    // s = clock_time() / CLOCK_SECOND;
-    // ms1 = (clock_time() % CLOCK_SECOND) * 10 / CLOCK_SECOND;
-    // ms2 = ((clock_time() % CLOCK_SECOND) * 100 / CLOCK_SECOND) % 10;
-    // ms3 = ((clock_time() % CLOCK_SECOND) * 1000 / CLOCK_SECOND) % 10;
-
-    counter_light++;
-    // printf(": %d (cnt) %d (ticks) %d.%d%d%d (sec) \n", counter_light, clock_time(), s, ms1, ms2, ms3);
-    // printf("Getting Light reading\n");
-    v = get_light_reading();
-    if (count_exec)
+    if (!lightChange)
     {
-        diff = abs(prevValue - v) / 100;
-        if (diff > 300)
-        {
-            lightChange = true;
-            return;
-        }
+        /* Re-arm rtimer. Starting up the sensor takes around 125ms */
+        rtimer_set(&light_timer, RTIMER_NOW() + timeout_light, 0, do_light, NULL);
+        get_light_reading();
     }
-    prevValue = v;
-    count_exec += 1;
 }
 
 static void
@@ -165,16 +138,13 @@ get_mpu_reading(int arr[])
     //   printf(" G\n");
 }
 
-static int
-get_light_reading()
+static void get_light_reading()
 {
-    int value;
 
     value = opt_3001_sensor.value(0);
     if (value != CC26XX_SENSOR_READING_ERROR)
     {
-        printf("OPT: Light=%d.%02d lux\n", value / 100, value % 100);
-        return value;
+        // printf("OPT: Light=%d.%02d lux\n", value / 100, value % 100);
     }
     else
     {
@@ -199,16 +169,21 @@ init_mpu_reading(void)
 PROCESS_THREAD(process_rtimer, ev, data)
 {
     PROCESS_BEGIN();
-
+    static struct etimer E;
     init_mpu_reading();
-    init_opt_reading();
-
     if (process_is_running(&process_etimer))
     {
+        buzzer_stop();
         process_exit(&process_etimer);
+    }
+    if (process_is_running(&process_light))
+    {
+        process_exit(&process_light);
     }
 
     printf("Started IMU, in IDLE\n");
+    etimer_set(&E, 3 * CLOCK_SECOND);
+    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
     // process_event_t movement_detected_event = process_alloc_event();
     while (1)
     {
@@ -228,8 +203,8 @@ PROCESS_THREAD(process_rtimer, ev, data)
             // PROCESS_YIELD();
         }
     }
-    printf("Out of loop\n");
-    PROCESS_EXIT();
+    // printf("Out of loop\n");
+    PROCESS_YIELD();
 
     // process_start(&process_etimer, NULL);
 
@@ -252,6 +227,11 @@ PROCESS_THREAD(process_etimer, ev, data)
 
     while (1)
     {
+        if (lightChange)
+        {
+            buzzer_stop();
+            break;
+        }
         // PROCESS_WAIT_EVENT_UNTIL(ev == movement_detected_event);
         buzzer_start(buzzerFrequency);
         etimer_set(&timer_etimer, 3 * CLOCK_SECOND);
@@ -260,7 +240,7 @@ PROCESS_THREAD(process_etimer, ev, data)
         etimer_set(&timer_etimer, 3 * CLOCK_SECOND);
         PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
     }
-
+    PROCESS_YIELD();
     PROCESS_END();
 }
 
@@ -274,24 +254,41 @@ PROCESS_THREAD(process_light, ev, data)
     }
     // process_start(&process_etimer, NULL);
     init_opt_reading();
+    static struct etimer et;
     while (1)
     {
         if (lightChange)
         {
-            printf("Light change detected\n");
             lightChange = false;
+            printf("Light change detected\n");
             count_exec = 0;
             process_exit(&process_etimer);
-            process_start(&process_rtimer, NULL);
             printf("Out of the Light process, going to IDLE state\n");
             break;
         }
         else
         {
+            etimer_set(&et, CLOCK_SECOND);
+            PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
             rtimer_set(&light_timer, RTIMER_NOW() + timeout_light, 0, do_light, NULL);
-            PROCESS_YIELD();
+
+            // PROCESS_YIELD();
+            if (count_exec > 0)
+            {
+
+                diff = abs(value - prevValue);
+                printf("diff: %d\n", diff);
+
+                if (diff > 300)
+                {
+                    lightChange = true;
+                }
+            }
+            prevValue = value;
+            count_exec += 1;
         }
     }
-    PROCESS_EXIT();
+    process_start(&process_rtimer, NULL);
+    PROCESS_YIELD();
     PROCESS_END();
 }
